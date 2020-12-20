a67fdfbfcab2e07383315a00dbb150f480378a67
VinuBalagopalAP <vinubalagopalap@gmail.com>
2020-12-20 17:49:21 +0530

    Building a Custom List Item

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,21 @@ class MyHomePage extends StatelessWidget {
               Column(
                 children: transactions.map((tx) {
                   return Card(
-                    child: Text(tx.title),
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                            child: Text(
+                          tx.amount.toString(),
+                        )),
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              tx.date.toString(),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
                   );
                 }).toList(),
               )
