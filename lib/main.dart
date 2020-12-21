e326665c55dcf893e3127e42dbbb2ca8acea5a5e
VinuBalagopalAP <vinubalagopalap@gmail.com>
2020-12-21 16:49:15 +0530

    DateFormat Patterns

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:PersonalExpensesApp/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -86,7 +87,7 @@ class MyHomePage extends StatelessWidget {
                               ),
                             ),
                             Text(
-                              tx.date.toString(),
+                              DateFormat.yMMMd().format(tx.date),
                               style: TextStyle(
                                 color: Colors.grey,
                               ),
